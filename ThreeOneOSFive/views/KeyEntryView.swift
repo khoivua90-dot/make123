335035b86080f37e6c3b96701709ce3c7472e816
conchimlenmen12 <canhsieuvip@gmail.com>
2026-08-22 02:54:59 +0700

    Rename to Cheating Store, new icon, switch to cjmp/ profile (Vassco)

--- ThreeOneOSFive/views/KeyEntryView.swift
+++ ThreeOneOSFive/views/KeyEntryView.swift
@@ -74,7 +74,7 @@ struct KeyEntryView: View {
             }
         }
         .safeAreaInset(edge: .bottom) {
-            Text("Make By ©PROXY VN CHEAT")
+            Text("Make By ©Cheating Store")
                 .font(.caption2.weight(.medium))
                 .foregroundStyle(.secondary)
                 .padding(.vertical, 14)
